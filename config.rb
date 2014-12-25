61f0c12a32ab083f0b2d962e28c5b814236c624a
Zack Siri <zack@codemy.net>
2014-12-25 17:57:58 +0700

    side nav working

--- config.rb
+++ config.rb
@@ -53,6 +53,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :fonts_dir,  'fonts-folder'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
